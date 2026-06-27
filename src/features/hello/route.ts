6c1447439308ee5700b5ce2888c752f8b2a43428
keigo <2364138keigo@gmail.com>
2026-06-27 23:20:51 +0900

    zodでバリデーションする

--- src/features/hello/route.ts
+++ src/features/hello/route.ts
@@ -1,15 +1,20 @@
 import { Hono } from "hono";
+import { validator } from "hono-openapi";
+import { z } from "zod";
 
 const helloRoute = new Hono();
+
+export const echoQuerySchema = z.object({
+  message: z.string().min(1).max(100),
+  name: z.string().min(1).max(100),
+});
 
 helloRoute.get("/hello", (c) => {
   return c.json({ Message: "Hello Hono!" });
 });
 
-helloRoute.get("/echo", (c) => {
-  // クエリパラメータの取得
-  const name = c.req.query("name");
-  const message = c.req.query("message");
+helloRoute.get("/echo", validator("query", echoQuerySchema), (c) => {
+  const { message, name } = c.req.valid("query");
 
   return c.json({ Message: `${message} ${name}!!!` });
 });
