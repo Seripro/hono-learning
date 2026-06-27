1de3a671cb33480ed30466970c15cba173196ee4
keigo <2364138keigo@gmail.com>
2026-06-27 15:22:54 +0900

    dotenv/configをimportし、環境変数を読み込む

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema.js";
 
 export const client = postgres(process.env.DATABASE_URL!);
 export const db = drizzle({ client, schema });
+console.log(process.env.DATABASE_URL);
